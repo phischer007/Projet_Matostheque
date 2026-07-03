a52b3e4e7b5e660be0bd48cb9e4209f83dfbfe51
Vikhram Duffour <vikhram-kofi.duffour@univ-grenoble-alpes.fr>
2026-07-03 15:42:39 +0200

    latest update

--- fronttheque/next.config.js
+++ fronttheque/next.config.js
@@ -1,12 +1,22 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   basePath: '/mutmat',
+  
+  // The 'env' object allows you to inject these variables 
+  // into your client-side code at build time.
   env: {
-    NEXT_PUBLIC_DJANGO_SERVER: process.env.NODE_ENV === 'production'
-      ? `https://${process.env.REACT_NEXT_ALLOWED_HOSTS}/api` 
-      : 'http://localhost:8030/api',
-
-    NEXT_PUBLIC_ASSETS: process.env.NODE_ENV === 'production'
-      ? `https://${process.env.REACT_NEXT_ALLOWED_HOSTS}/media/`
-      : 'http://localhost:8030/assets/',
+    NEXT_PUBLIC_DJANGO_SERVER: process.env.NEXT_PUBLIC_DJANGO_SERVER || (
+      process.env.NODE_ENV === 'production' 
+        ? `https://${process.env.NEXT_PUBLIC_ALLOWED_HOSTS}/api` 
+        : 'http://localhost:8030/api'
+    ),
+    
+    NEXT_PUBLIC_ASSETS: process.env.NEXT_PUBLIC_ASSETS || (
+      process.env.NODE_ENV === 'production' 
+        ? `https://${process.env.NEXT_PUBLIC_ALLOWED_HOSTS}/media/` 
+        : 'http://localhost:8030/assets/'
+    ),
   },
 };
+
+module.exports = nextConfig;
