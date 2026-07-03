8296550f0cfc363d9dc4c0eb0d8795c7a31ddbdb
Vikhram Duffour <vikhram-kofi.duffour@univ-grenoble-alpes.fr>
2026-07-03 15:47:54 +0200

    latest update

--- fronttheque/src/utils/config.example.js
+++ fronttheque/src/utils/config.example.js
@@ -1,7 +1,7 @@
-const apiUrl = process.env.REACT_APP_API_URL || (
- process.env.NODE_ENV === 'production' ?
- 'https://server_name.example.com/api'
- : 'http://localhost:8030/api'
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || (
+  process.env.NODE_ENV === 'production' ? 
+  `https://${process.env.NEXT_PUBLIC_ALLOWED_HOSTS}/api` : 
+  'http://localhost:8030/api'
 );
 
 const config = {
